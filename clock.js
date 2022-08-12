af69feed201c3f14760f4dd82e20e4dae11d2157
devin-lynch <devin.l.lynch@gmail.com>
2022-08-12 16:08:15 -0500

    Add functions for each hand

---
+++ clock.js
@@ -0,0 +1,19 @@
+hour = document.querySelector('#hour')
+minute = document.querySelector('#minute')
+second = document.querySelector('#second')
+
+// let degrees = 90
+
+// hour.style.transform = "rotate(" + degrees + "deg)"
+
+function secondRotation(s){
+    return = s * 360/60
+}
+
+function minuteRotation(m){
+    return = m * 360/60
+}
+
+function hourRotation(h){
+    return = h * 360/12
+}
